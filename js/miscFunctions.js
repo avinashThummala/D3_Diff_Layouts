feb4b4baa5bfbfcd49cd57c8464189bb37aa6bd2
Avinash Thummala <avinash.t.iiit@gmail.com>
2015-03-04 22:29:48 +0530

    First version of design based Viz

--- js/miscFunctions.js
+++ js/miscFunctions.js
@@ -31,3 +31,48 @@ function getStrWidth(str)
 
   return (test.clientWidth + 1);
 }
+
+function calculateMinMaxYears(obj)
+{
+  var pYear;
+
+  for(index in obj.years)
+  {
+    pYear=obj.years[index].year;
+
+    if(pYear<minYear) 
+      minYear=pYear;
+
+    if(pYear>maxYear)
+      maxYear=pYear;
+  }
+
+}
+
+function modifyLabels()
+{
+  document.getElementById('minLabel').innerHTML=minYear;
+  document.getElementById('maxLabel').innerHTML=maxYear;
+
+  document.getElementById('tSlider').setAttribute("min", minYear);
+  document.getElementById('tSlider').setAttribute("max", maxYear);
+}
+
+function fillList() {
+
+  var mList = document.getElementById('mList');
+
+  fullCNameList.map(function(cName, index){
+
+    var opt = document.createElement('option');
+
+    opt.selected = true;
+    opt.value = selectedIds[index];
+    opt.name = cName;
+    opt.innerHTML = cName;
+
+    mList.appendChild(opt);
+
+  })
+
+}
